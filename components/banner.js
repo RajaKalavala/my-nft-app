cfe656b1e7f3871926860198f63229e2c37ee625
RajaKalavala <kalavala.raja@siemens-healthineers.com>
2022-03-18 23:19:02 +0530

    Add Banner.js basic data and style names

--- components/banner.js
+++ components/banner.js
@@ -1,5 +1,16 @@
+import styles from "./banner.module.css";
+
 const Banner = () => {
-  return <div> Banner </div>;
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>
+        <span className={styles.title1}>NFT </span>
+        <span className={styles.title2}>App</span>
+      </h1>
+      <p className={styles.subTitle}>Discover and Create new NFTs</p>
+      <button className={styles.button}>Mint NFT</button>
+    </div>
+  );
 };
 
 export default Banner;
