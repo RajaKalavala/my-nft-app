5ae09fc20996cf83eeb8650ff1e4fda0b2567592
RajaKalavala <kalavala.raja@siemens-healthineers.com>
2022-03-19 21:53:34 +0530

    Test checkin for Vercel git

--- components/banner.js
+++ components/banner.js
@@ -4,7 +4,7 @@ const Banner = (props) => {
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>
-        <span className={styles.title1}>Non Fungible Token</span>
+        <span className={styles.title1}>Non Fungible Tokens</span>
         <span className={styles.title2}>App</span>
       </h1>
       <p className={styles.subTitle}>Discover and Create new NFTs</p>
