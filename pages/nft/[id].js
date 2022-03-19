440fc1ff19e905d239f44d0389f2f5313af076c6
RajaKalavala <kalavala.raja@siemens-healthineers.com>
2022-03-19 22:02:04 +0530

    [DynamicRouting] Implement Link from next/link

--- pages/nft/[id].js
+++ pages/nft/[id].js
@@ -1,8 +1,16 @@
+import Link from "next/link";
 import { useRouter } from "next/router";
 
 const NFT = () => {
   const router = useRouter();
-  return <div>NFT {router.query.id}</div>;
+  return (
+    <div>
+      NFT {router.query.id}
+      <Link href="/">
+        <a>Back To Home</a>
+      </Link>
+    </div>
+  );
 };
 
 export default NFT;
