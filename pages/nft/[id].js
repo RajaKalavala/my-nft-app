2d9cc753981c7a2321dfd1fcc8df58dfc8ddcf8a
RajaKalavala <kalavala.raja@siemens-healthineers.com>
2022-03-21 22:05:03 +0530

    [StaticSiteGeneration] Use getStaticProps and getStaticpaths to read id from params and get the data of that product

--- pages/nft/[id].js
+++ pages/nft/[id].js
@@ -1,14 +1,37 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
 
-const NFT = () => {
+import NFTsData from "../../data/nft-data.json";
+
+export function getStaticProps(staticProps) {
+  const params = staticProps.params;
+  console.log("params - ", params);
+  return {
+    props: {
+      nft: NFTsData.find((nft) => {
+        return nft.id.toString() === params.id;
+      }),
+    },
+  };
+}
+
+export function getStaticPaths() {
+  return {
+    paths: [{ params: { id: "0" } }, { params: { id: "1" } }],
+    fallback: false,
+  };
+}
+
+const NFT = (props) => {
   const router = useRouter();
+  console.log("props NFT", props);
   return (
     <div>
       NFT {router.query.id}
       <Link href="/">
         <a>Back To Home</a>
       </Link>
+      <p>{props.nft.name}</p>
     </div>
   );
 };
